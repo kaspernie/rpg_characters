56dfaf5d6f8f8eb826d5929e2d4744ec8baa599f
kaspernie <kaspernielsen@gmail.com>
2022-02-16 20:17:31 +0100

    feature: added Test dir again with mock tests

---
+++ Test/com/github/kaspernie/rpgchars/HeroTest.java
@@ -0,0 +1,24 @@
+package com.github.kaspernie.rpgchars;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getLevel() {
+    }
+
+    @Test
+    void setLevel() {
+    }
+}
